c569d0399d6ef785f374f0d3cb776af7ca81be8c
Berk Ali Demir <101639906+B1gF1sh@users.noreply.github.com>
2023-06-12 05:00:37 +0300

    Update DBconnection.java

--- src/library_management_systemN/DBconnection.java
+++ src/library_management_systemN/DBconnection.java
@@ -5,7 +5,7 @@ public class DBconnection {
 	
 	 static final String DB_URL = "jdbc:mysql://localhost:3306/library_management_system";
 	 static final String USER = "root";
-	 static final String PASS = "Mertyanki2004";
+	 static final String PASS = "";
 	 
 	 
 
